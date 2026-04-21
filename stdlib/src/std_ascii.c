9170c309a6578b3739ed421f958141c6063bea8c
Matti Laamanen <mattilaa@gmail.com>
2026-04-21 19:05:53 +0300

    Optimize braille glyph generation for terminal demo

---
+++ stdlib/src/std_ascii.c
@@ -0,0 +1,33 @@
+#include <stdint.h>
+
+#if defined(__STDC_VERSION__) && (__STDC_VERSION__ >= 201112L)
+#define MLANG_ASCII_TLS _Thread_local
+#else
+#define MLANG_ASCII_TLS
+#endif
+
+static MLANG_ASCII_TLS char g_mlang_ascii_braille_bufs[8][5];
+static MLANG_ASCII_TLS int g_mlang_ascii_braille_idx = 0;
+
+static char* next_braille_buf(void)
+{
+    g_mlang_ascii_braille_idx = (g_mlang_ascii_braille_idx + 1) & 7;
+    return g_mlang_ascii_braille_bufs[g_mlang_ascii_braille_idx];
+}
+
+const char* __mlang_std_ascii_braille_mask(int32_t mask)
+{
+    if(mask < 0)
+        mask = 0;
+    if(mask > 255)
+        mask = 255;
+
+    uint32_t cp = 0x2800u + (uint32_t)mask;
+    char* out = next_braille_buf();
+
+    out[0] = (char)(0xE0u | ((cp >> 12) & 0x0Fu));
+    out[1] = (char)(0x80u | ((cp >> 6) & 0x3Fu));
+    out[2] = (char)(0x80u | (cp & 0x3Fu));
+    out[3] = '\0';
+    return out;
+}
